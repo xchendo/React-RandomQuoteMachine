edd8d71ceecdc66a8597bac71649b5bee586b664
George <loaizageorge@gmail.com>
2018-01-03 22:29:15 -0500

    Styled display quote view

--- app/components/QuoteDisplay.jsx
+++ app/components/QuoteDisplay.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function QuoteDisplay(props) {
   return(
     <div className = "center quoteContainer callout primary small small-10 medium-6">
-      <i className="fa fa-quote-left quoteButton" aria-hidden="true"></i>
+      <i className="fa fa-quote-left quote--icon" aria-hidden="true"></i>
       <blockquote>
       <p className = "quote">{props.quote}</p>
       <cite>{props.author}</cite>
